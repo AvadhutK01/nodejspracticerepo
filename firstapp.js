2139bea07523b0b86bb7332e146c2c1dd2b60472
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-09-19 12:13:19 +0530

    Arrow function demo

--- firstapp.js
+++ firstapp.js
@@ -1,3 +1,10 @@
-const fs = require("fs");
-fs.writeFileSync("hello.txt", "hello world")
-console.log("hello world");
+// const fs = require("fs");
+// fs.writeFileSync("hello.txt", "hello world")
+// console.log("hello world");
+
+//Arrow functions
+
+let result = (a, b) => {
+    return a * b;
+}
+console.log(result(12, 14))
