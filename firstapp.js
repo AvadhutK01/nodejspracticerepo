1cc994d7ac6be96d5ede406e49d7adcf48f1bee9
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-09-19 13:22:17 +0530

    Destructuring arrays and objects demo

--- firstapp.js
+++ firstapp.js
@@ -31,16 +31,34 @@
 
 //Rest and Spread Operators
 
-const object1 = { 'key1': 1 }
-const object2 = { ...obj1 }
-if (object2 === object1) {
-    console.log('same objects')
-}
-else {
-    console.log('different objects')
-}
+// const object1 = { 'key1': 1 }
+// const object2 = { ...obj1 }
+// if (object2 === object1) {
+//     console.log('same objects')
+// }
+// else {
+//     console.log('different objects')
+// }
 
-const obj1 = { 'key1': 1, 'key2': 2 }
-const obj2 = { ...obj1, key1: 1000 }
-console.log(obj1)
-console.log(obj2)
+// const obj1 = { 'key1': 1, 'key2': 2 }
+// const obj2 = { ...obj1, key1: 1000 }
+// console.log(obj1)
+// console.log(obj2)
+
+//Destructuring arrays and objects
+
+const obj1 = { 'key1': 1, "key2": 2, "key3": 1000 }
+const { key1, key3 } = { ...obj1 }
+console.log(key1, key3)
+
+const arr1 = ['value1', 'value2']
+
+const [val1, val2] = arr1
+console.log(val1)
+console.log(val2)
+
+const obj2 = { 'key1': 1, "key2": 2, "key3": 1000 }
+let { keys1, keys3 } = obj1
+keys1 = 20;
+keys3 = 123
+console.log(obj2.key1, obj2.key3)
