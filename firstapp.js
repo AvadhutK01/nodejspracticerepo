989195c7160600a0dc92e3e3d38b903a11ebdfa7
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-09-19 13:09:21 +0530

    Spread and rest operator demo

--- firstapp.js
+++ firstapp.js
@@ -11,12 +11,36 @@
 
 //Objects
 
-let student = {
-    name: "Avadhut",
-    age: 21,
-    DOB: "6/6/2003",
-    data: function () {
-        console.log(`Name: ${this.name},Age: ${this.age},Dob:${this.DOB}`);
-    }
-};
-student.data();
+// let student = {
+//     name: "Avadhut",
+//     age: 21,
+//     DOB: "6/6/2003",
+//     data: function () {
+//         console.log(`Name: ${this.name},Age: ${this.age},Dob:${this.DOB}`);
+//     }
+// };
+// student.data();
+
+//Arrays
+
+// const hobbies = ['apple', 'oranges', ' ', 'mango', ' ', 'lemon'];
+// console.log(hobbies.map((v) => {
+//     return v == ' ' ? 'empty string' : v;
+// }));
+// console.log(arr);
+
+//Rest and Spread Operators
+
+const object1 = { 'key1': 1 }
+const object2 = { ...obj1 }
+if (object2 === object1) {
+    console.log('same objects')
+}
+else {
+    console.log('different objects')
+}
+
+const obj1 = { 'key1': 1, 'key2': 2 }
+const obj2 = { ...obj1, key1: 1000 }
+console.log(obj1)
+console.log(obj2)
