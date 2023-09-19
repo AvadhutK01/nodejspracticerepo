6c0d890974f62d6089cedc10e571a49557c65783
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-09-19 13:33:33 +0530

    sync code and promises demo

--- firstapp.js
+++ firstapp.js
@@ -47,18 +47,61 @@
 
 //Destructuring arrays and objects
 
-const obj1 = { 'key1': 1, "key2": 2, "key3": 1000 }
-const { key1, key3 } = { ...obj1 }
-console.log(key1, key3)
+// const obj1 = { 'key1': 1, "key2": 2, "key3": 1000 }
+// const { key1, key3 } = { ...obj1 }
+// console.log(key1, key3)
 
-const arr1 = ['value1', 'value2']
+// const arr1 = ['value1', 'value2']
 
-const [val1, val2] = arr1
-console.log(val1)
-console.log(val2)
+// const [val1, val2] = arr1
+// console.log(val1)
+// console.log(val2)
 
-const obj2 = { 'key1': 1, "key2": 2, "key3": 1000 }
-let { keys1, keys3 } = obj1
-keys1 = 20;
-keys3 = 123
-console.log(obj2.key1, obj2.key3)
+// const obj2 = { 'key1': 1, "key2": 2, "key3": 1000 }
+// let { keys1, keys3 } = obj1
+// keys1 = 20;
+// keys3 = 123
+// console.log(obj2.key1, obj2.key3)
+
+//async code and promises
+
+// console.log('a');
+
+// console.log('b');
+
+// setTimeout(() => console.log('c'), 3000)
+
+// console.log('d');
+
+// console.log('a');
+
+// console.log('b');
+
+// setTimeout(() => console.log('c'), 3000)
+
+// setTimeout(() => console.log('d'), 0)
+
+// console.log('e');
+
+
+
+
+async function func() {
+    console.log('a');
+    console.log('b');
+
+    await new Promise(resolve => setTimeout(() => {
+        console.log('c');
+        resolve();
+    }, 3000));
+    await new Promise(resolve => setTimeout(() => {
+        console.log('d');
+        resolve();
+    }, 0));
+    console.log('e');
+
+}
+func()
+
+
+
