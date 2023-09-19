83dd2d464d93a6dd56f79f707cd7b60bf0a8a795
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-09-19 12:28:39 +0530

    Object demo

--- firstapp.js
+++ firstapp.js
@@ -4,7 +4,19 @@
 
 //Arrow functions
 
-let result = (a, b) => {
-    return a * b;
-}
-console.log(result(12, 14))
+// let result = (a, b) => {
+//     return a * b;
+// }
+// console.log(result(12, 14))
+
+//Objects
+
+let student = {
+    name: "Avadhut",
+    age: 21,
+    DOB: "6/6/2003",
+    data: function () {
+        console.log(`Name: ${this.name},Age: ${this.age},Dob:${this.DOB}`);
+    }
+};
+student.data();
